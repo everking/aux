5ab17ff9108f02eb720e9a03cf486117213bbd2a
Eric de Guzman <eric.deguzman@outlook.com>
2024-04-20 05:45:34 -0700

    Parameterize image name

--- docker-run.sh
+++ docker-run.sh
@@ -3,9 +3,14 @@ docker rm aux
 
 echo "HOME=$HOME/dev/aux-posts"
 
+export IMAGE=ghcr.io/everking/aux:latest
+docker pull ${IMAGE}
+
+echo "Run ${IMAGE}"
+
 docker run -d --name aux -it -p 5173:5173 \
   -v $HOME/dev/aux-posts:/app/src/lib/posts \
-  ghcr.io/everking/aux bash
+  ${IMAGE} bash
 
 docker logs -f aux
 
