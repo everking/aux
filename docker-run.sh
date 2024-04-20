61369f91086342dd82c9e64ab0b46d486b8f1127
Eric de Guzman <eric.deguzman@outlook.com>
2024-04-20 05:16:50 -0700

    Run container in background.

--- docker-run.sh
+++ docker-run.sh
@@ -3,6 +3,9 @@ docker rm aux
 
 echo "HOME=$HOME/dev/aux-posts"
 
-docker run --name aux -it -p 5173:5173 \
+docker run -d --name aux -it -p 5173:5173 \
   -v $HOME/dev/aux-posts:/app/src/lib/posts \
   ghcr.io/everking/aux bash
+
+docker logs -f aux
+
