1d8a323b660217a4204a311f79f4cea600703d83
Eric de Guzman <eric.deguzman@outlook.com>
2024-05-12 18:35:10 -0700

    Add email env

--- docker-run.sh
+++ docker-run.sh
@@ -8,7 +8,23 @@ docker pull ${IMAGE}
 
 echo "Run ${IMAGE}"
 
-docker run -d --name aux -it -p 5173:5173 \
+if [ -z "${SMTP_EMAIL_USER}" ]
+then
+  echo "SMTP_EMAIL_USER variable required."
+  exit 1
+fi
+
+if [ -z "${SMTP_EMAIL_PASSWORD}" ]
+then
+  echo "SMTP_EMAIL_PASSWORD variable required."
+  exit 1
+fi
+
+
+docker run -d --name aux \
+  -e SMTP_EMAIL_PASSWORD=${SMTP_EMAIL_PASSWORD} \
+  -e SMTP_EMAIL_USER=${SMTP_EMAIL_USER} \
+  -it -p 5173:5173 \
   -v $HOME/dev/aux-posts:/app/src/lib/posts \
   ${IMAGE} bash
 
