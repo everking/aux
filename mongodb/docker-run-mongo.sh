9a8ec1a68a893bce6761a6d2347dbca15aa0e23e
Eric de Guzman <eric.deguzman@outlook.com>
2024-05-19 07:32:33 -0700

    Mongo docker run update

--- mongodb/docker-run-mongo.sh
+++ mongodb/docker-run-mongo.sh
@@ -1,9 +1,17 @@
 export MONGO_CONTAINER=mongodb
 
-mkdir -p $HOME/dev/${MONGO_CONTAINER}
+if [ -z "${MONGO_FOLDER}" ]
+then
+  export MONGO_FOLDER="$HOME/dev/${MONGO_CONTAINER}_data"
+fi
+
+echo "MongoDB folder: ${MONGO_FOLDER}"
+mkdir -p ${MONGO_FOLDER}
 
 docker run -d --name ${MONGO_CONTAINER} \
   -p 27017:27017 \
-  -v $HOME/dev/${MONGO_CONTAINER}:/data/db mongo
+  -u root \
+  -v ${MONGO_FOLDER}:/data \
+  mongo
 
 docker logs -f ${MONGO_CONTAINER}
--- mongodb/reset.sh
+++ mongodb/reset.sh
@@ -1,11 +1,18 @@
 export MONGO_CONTAINER=mongodb
+
+if [ -z "${MONGO_FOLDER}" ]
+then
+  export MONGO_FOLDER="$HOME/dev/${MONGO_CONTAINER}_data"
+fi
+
 echo "kill ${MONGO_CONTAINER}"
 docker kill ${MONGO_CONTAINER}
+
 echo "rm ${MONGO_CONTAINER}"
 docker rm ${MONGO_CONTAINER} 
 
-echo "rm -rf $HOME/dev/${MONGO_CONTAINER}"
-rm -rf $HOME/dev/${MONGO_CONTAINER}
+echo "rm -rf ${MONGO_FOLDER}"
+rm -rf ${MONGO_FOLDER}
 
-echo "mkdir -p $HOME/dev/${MONGO_CONTAINER}"
-mkdir -p $HOME/dev/${MONGO_CONTAINER}
+echo "mkdir -p ${MONGO_FOLDER}"
+mkdir -p ${MONGO_FOLDER}
