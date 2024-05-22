a08a672c227acb9a6fb7fc3bf7eae61a4ab23613
Eric de Guzman <eric.deguzman@outlook.com>
2024-05-22 10:36:12 -0700

    Fix migrate

--- mongodb/scripts/migrate.js
+++ mongodb/scripts/migrate.js
@@ -4,7 +4,9 @@ const path = require('path');
 const { marked } = require('marked');
 const yaml = require('js-yaml');
 
-mongoose.connect('mongodb://localhost/aux', {
+const MONGO_HOST=process.env.MONGO_HOST ? process.env.MONGO_HOST : "localhost";
+console.log(`MONGO_HOST: ${MONGO_HOST}`);
+mongoose.connect(`mongodb://${MONGO_HOST}/aux`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -46,40 +48,44 @@ const mdToHtml = (mdString) => {
   }
 };
 
-const basePath = "/Users/edeguzma/personal-dev/aux/src/lib/posts"
-// Get the list of all .md files in the /data/ folder
-const mdFiles = fs.readdirSync(basePath).filter(file => path.extname(file) === '.md');
-
-// Process each .md file
-mdFiles.forEach((file) => {
-  const filePath = path.join(basePath, file);
-  const articleId = file.replace(".md", "");
-  const fileContent = fs.readFileSync(filePath, 'utf-8');
-
-  console.log(`articleId: ${articleId}`);
+const AUX_HOME=process.env.AUX_HOME;
+if (AUX_HOME) {
+  const basePath = `${AUX_HOME}/src/lib/posts`
+  // Get the list of all .md files in the /data/ folder
+  const mdFiles = fs.readdirSync(basePath).filter(file => path.extname(file) === '.md');
   
-  // Split the file content into YAML and Markdown parts
-  const parts = fileContent.split('---');
-  const yamlString = parts[1];
-  const mdString = parts.slice(2).join('---');
-
-  // Convert YAML to JSON and Markdown to HTML
-  const jsonString = yamlToJson(yamlString);
-  const articleObject = JSON.parse(jsonString);
-  articleObject.body = mdString;
-  articleObject.articleId = articleId;
-  articleObject.date = new Date();
-  articleObject.updated = new Date();
-
-  const article = new Article(articleObject);
-  console.log(`\narticleObject: ${JSON.stringify(articleObject, null, 2)}`);
-  (async ()=> {
-    try {
-      await article.save();
-    } catch (error) {
-      console.log(error);
-    }
-  })();
-});
-
+  // Process each .md file
+  mdFiles.forEach((file) => {
+    const filePath = path.join(basePath, file);
+    const articleId = file.replace(".md", "");
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+  
+    console.log(`articleId: ${articleId}`);
+    
+    // Split the file content into YAML and Markdown parts
+    const parts = fileContent.split('---');
+    const yamlString = parts[1];
+    const mdString = parts.slice(2).join('---');
+  
+    // Convert YAML to JSON and Markdown to HTML
+    const jsonString = yamlToJson(yamlString);
+    const articleObject = JSON.parse(jsonString);
+    articleObject.body = mdString;
+    articleObject.articleId = articleId;
+    articleObject.date = new Date();
+    articleObject.updated = new Date();
+  
+    const article = new Article(articleObject);
+    console.log(`\narticleObject: ${JSON.stringify(articleObject, null, 2)}`);
+    (async ()=> {
+      try {
+        await article.save();
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  });  
+} else {
+  console.log("Required AUX_HOME variable not found.")
+}
 console.log("End!");
